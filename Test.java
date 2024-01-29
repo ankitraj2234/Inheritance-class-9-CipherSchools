b4d0b85aec088d50ccd0221a0d8d3191dd1d93c7
ankitraj_2234 <106026385+ankitraj2234@users.noreply.github.com>
2024-01-29 20:05:07 +0530

    Test.java

---
+++ Test.java
@@ -0,0 +1,36 @@
+class A {
+    int a,b;
+    public void show(){
+        System.out.println("Hello!");
+    }
+}
+
+interface B{
+    public void eat();
+}
+
+interface D extends B{
+    public void sleep();
+}
+
+class C implements B, D{
+    int e,f;
+    public void speak(){
+        System.out.println("Speaking!");
+    }
+    @Override
+    public void eat() {
+        
+    }
+    @Override
+    public void sleep() {
+        
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+     B b = new B();
+     C c = new C();   
+    }
+}
